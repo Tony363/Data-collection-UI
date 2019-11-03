86d2ef4af532c949cea85a65dd9093254e53dcf8
Tony <pysolver33@gmail.com>
2019-11-03 23:14:15 +0800

    todays lesson code so far

--- first_lesson.py
+++ first_lesson.py
@@ -4,20 +4,42 @@ import xgboost as xgb
 # seed random number generator
 seed(1)
 # generate random numbers between 0-1
-column = []
-for _ in range(100):
+value_1 = []
+value_2 = []
+dataset1 = [value_1,value_2]
+dataset2 = []
+
+
+# column = []
+# for _ in range(10):
+#     row = []
+#     x1 = random()
+#     x2 = random()
+#     row.append(round(x1,2))
+#     row.append(round(x2,2))
+#     if x1 + x2 > 1:
+#         z = 1
+#         row.append(z)
+#     else:
+#         z = 0
+#         row.append(z)
+#     print(row)
+#     column.append(row)
+# print(column)
+
+for _ in range(10):
     row = []
     x1 = random()
     x2 = random()
-    row.append(round(x1,2))
-    row.append(round(x2,2))
-    if x1 + x2 > 1:
-        z = 1
-        row.append(z)
-    else:
-        z = 0
-        row.append(z)
-    print(row)
-    column.append(row)
-print(column)
+    value_1.append(x1)
+    value_2.append(x2)
+    # if x1 + x2 > 1:
+    #     z = 1
+    #     dataset3.append(z)
+    # else:
+    #     z = 0
+    #     dataset3.append(z)
+print(dataset1)
+
+# x = column[]
 
