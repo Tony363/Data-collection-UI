db213c50f0226eef158aebd38e7be67305f1f76a
Tony <pysolver33@gmail.com>
2019-11-03 13:57:38 +0800

    organized some code

--- xgboost.py
+++ xgboost.py
@@ -3,25 +3,20 @@ from random import random
 # seed random number generator
 seed(1)
 # generate random numbers between 0-1
-
-dataset1 = []
-dataset2 = []
-dataset3 = []
-dataset = [dataset1, dataset2,dataset3]
-
+column = []
 for _ in range(100):
-	row = []
+    row = []
     x1 = random()
     x2 = random()
+    row.append(x1)
+    row.append(x2)
     if x1 + x2 > 1:
         z = 1
         row.append(z)
     else:
         z = 0
         row.append(z)
-    
-    row.append(x1)
-    row.append(x2)
-    # print(round(x1,2),round(x2,2),z)
     print(row)
+    column.append(row)
+print(column)
 
