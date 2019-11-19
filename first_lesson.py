95f6f3d371e60ca4b809482cb5a50286e9c8e287
Tony Siu <48950649+thefool363@users.noreply.github.com>
2019-11-20 00:48:16 +0800

    FUCK XGBOOST WON'T TRAIN MY DATASET!

--- first_lesson.py
+++ first_lesson.py
@@ -8,12 +8,15 @@ This is a temporary script file.
 #######bst.predict()
 from random import seed
 from random import random
+from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score
 import numpy as np
 import xgboost as xgb
 import operator
 
-f=open("dump.raw.txt","w+")
-g=open("featmap.txt","w+")
+# f=open("dump.raw.txt","w+")
+# g=open("featmap.txt","w+")
 # A=[[random(),random()] for i in range(10)]
 
 seed(1)
@@ -21,7 +24,7 @@ seed(1)
 X=[]
 Y=[]
 
-for _ in range(10):
+for _ in range(100):
     row=[]
     x1 = random()
     x2 = random()
@@ -39,18 +42,24 @@ for _ in range(10):
         Y.append(z)
 
 
-print(X)
-print(Y)
+# print(X)
+# print(Y)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
 
 
 #need to get list first item and sort it
-X_sorted = sorted(X, key=operator.itemgetter(0))
-print(X_sorted)
+# X_sorted = sorted(X, key=operator.itemgetter(0))
+# print(X_sorted)
 
 #remove a value from the list
 
-dtrain=xgb.DMatrix(X[0:70],label=Y[0:70])
-dtest=xgb.DMatrix(X[71:100],label=Y[71:100])
+dtrain = xgb.DMatrix(X[0:70],label=Y[0:70])
+dtest = xgb.DMatrix(X[71:100],label=Y[71:100])
+
+
+
+dtrain_svm = xgb.DMatrix('dtrain.svm')
 dtest_svm = xgb.DMatrix('dtest.svm')
 
 param = {
@@ -66,23 +75,35 @@ num_round = 5
 bst = xgb.train(param, dtrain, num_round, evallist)
 preds = bst.predict(dtest)
 best_preds = np.asarray([np.argmax(line) for line in preds])
+print(bst)
+
+bst.save_model('0001.model')
+bst.dump_model('dump.raw.txt')
+
 
 bst_svm = xgb.train(param, dtrain_svm, num_round)
 preds_ = bst.predict(dtest_svm)
+best_preds_svm = [np.argmax(line) for line in preds_]
 
-bst.save_model('0001.model')
-#The model and its feature map can also be dumped to a text file.
+bst_svm.save_model('0002.model')
+bst_svm.dump_model('dump_svm.raw.txt','featmap.txt')
 
-# dump model
-bst.dump_model('dump.raw.txt')
-# dump model with feature map
-bst.dump_model('dump.raw.txt', 'featmap.txt')
 #A saved model can be loaded as follows:
-f.close
-g.close
+bst = xgb.Booster({'nthread': 4})  # init model
+bst.load_model('model.bin')  # load data
+# f.close
+# g.close
+
+# save the models for later
+# joblib.dump(bst, 'bst_model.pk1', compress=True)
+# joblib.dump(bst_svm, 'bst_svm_model.pk1', compress=True)
+
+print("Numpy array precision:{}".format(precision_score(Y_test, best_preds )))
+    
+print("Svm file precision:{}".format(precision_score(Y_test, best_preds_svm)) )
 
 # bst = xgb.Booster({'nthread': 4})  # init model
-bst.load_model('model.bin')  # load data
+# bst.load_model('model.bin')  # load data
 
 
 
