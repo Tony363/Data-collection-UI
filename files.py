02d5ace0511449e1b53c3b89df9b88d54553e19c
Tony Siu <48950649+thefool363@users.noreply.github.com>
2019-11-22 10:52:04 +0800

    having troubles with different files

--- files.py
+++ files.py
@@ -4,7 +4,7 @@ for filename in os.listdir(INPUT_DIR):
    with open(os.path.join(INPUT_DIR,filename), dialect='excel-tab') as infile:
       reader = csv.reader(infile)
       for row in reader:
-          print row
+          print(row)
           
 with open(..., 'w', newline='') as myfile:
    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
