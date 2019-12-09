33a7153b6c412d98111dc4967308ff943e0f6620
Tony Siu <thefool363@gmail.com>
2019-12-09 15:50:58 +0800

    sockets sockets

--- Python_server_sockets/echo_server.py
+++ Python_server_sockets/echo_server.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
 import socket
+import sys
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT =  65432      # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     
-
-    s.bind((HOST, PORT))
-    s.listen()
+   
+    try:
+        s.bind((HOST, PORT))        
+        s.settimeout(4)
+        s.listen()
+    except socket.timeout:
+        # s.close()
+        print('no client received')
+    
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
@@ -18,3 +25,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if not data:
                 break
             conn.sendall(data)
+    
+        
--- Python_server_sockets/udp_server.py
+++ Python_server_sockets/udp_server.py
@@ -3,32 +3,36 @@ import socket
 import sys
 import os
 
-def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('', 12000))
+# def main():
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('', 12000))
+print('waiting for udp client')
+server_socket.settimeout(5)
 
-    # for i in range(5):
-    while True:
-        try:
-            # rand = random.randint(0, 10)
-            message, address = server_socket.recvfrom(1024)
-            # message = message.upper()
-            message = b'message returned'
-            print(message)
-            print(address)
-            # if rand >= 4:
-            server_socket.sendto(message, address)
-        except KeyboardInterrupt:
-            server_socket.shutdown(socket.SOCK_DGRAM)
-            sys.exit()
-
-
-if __name__ == '__main__':
+while True:
     try:
-        main()
+       
+        message, address = server_socket.recvfrom(1024)
+        print('client received: {}'.format(address))
+        # message = message.upper()
+        send_message = b'connection established'
+        print(message)
+        
+        server_socket.sendto(send_message, address)
     except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+        print('time to close udp server')
+        server_socket.shutdown(socket.SOCK_DGRAM)
+        sys.exit(1)
+        
+            
+
+
+# if __name__ == '__main__':
+#     try:
+#         main()
+#     except KeyboardInterrupt:
+#         print('Interrupted')
+#         try:
+#             sys.exit(0)
+#         except SystemExit:
+#             os._exit(0)
