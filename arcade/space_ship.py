5638cb0d44b33856fcb8e6838db6f8d930a7a3d7
tony363 <pysolver33@gmail.com>
2020-03-10 11:04:07 +0800

    still gotta figure out the rotaton

--- arcade/space_ship.py
+++ arcade/space_ship.py
@@ -14,22 +14,25 @@ import random
 import arcade
 import os
 import math
+import sys
+import time
 from math import sqrt
 
-SPRITE_SCALING = 0.5
-SPRITE_SCALING_COIN = 0.2
+SPRITE_SCALING = 1
+SPRITE_SCALING_COIN = 0.5
+SPRITE_SCALING_LASER = 0.8
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Move Sprite by Angle Example"
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 1200
 
-MOVEMENT_SPEED = 5
+SCREEN_TITLE = "run for your life"
+
+MOVEMENT_SPEED = 6
 ANGLE_SPEED = 5
-
-COIN_COUNT = 5
-COIN_SPEED = 0.5
-
-SHIP_SPEED = 1
+BULLET_SPEED = 10
+SHIP_SPEED = 4
+COIN_COUNT = 0
+COIN_SPEED = 1
 
 class Coin(arcade.Sprite):
     """
@@ -77,6 +80,7 @@ class Coin(arcade.Sprite):
             # and change_y. Velocity is how fast the bullet travels.
             self.change_x = math.cos(angle) * COIN_SPEED
             self.change_y = math.sin(angle) * COIN_SPEED
+
     def get_coor(self):
         return self.coor_hist
  
@@ -105,6 +109,87 @@ class Player(arcade.Sprite):
         self.center_x += -self.speed * math.sin(angle_rad)
         self.center_y += self.speed * math.cos(angle_rad)
 
+
+
+    def follow_mouse(self,val_x,val_y):
+
+        start_x = self.center_x
+        start_y = self.center_y
+
+        # Get the destination location for the bullet
+        try:
+            dest_x = val_x
+            dest_y = val_y
+
+            # Do math to calculate how to get the bullet to the destination.
+            # Calculation the angle in radians between the start points
+            # and end points. This is the angle the bullet will travel.
+            x_diff = dest_x - start_x
+            y_diff = dest_y - start_y
+            angle = math.atan2(y_diff, x_diff)
+
+            # Taking into account the angle, calculate our change_x
+            # and change_y. Velocity is how fast the bullet travels.
+            self.change_x = math.cos(angle) * SHIP_SPEED
+            self.change_y = math.sin(angle) * SHIP_SPEED
+
+            self.center_x += self.change_x
+            self.center_y += self.change_y
+
+            # angle = 360-math.atan2(dest_x-300,dest_y-400)*180/math.pi
+
+            # self.angle = angle
+        except Exception as e:
+            pass
+
+
+
+
+class Triangle:
+    def __init__(self,val_x,val_y,color):
+        self.center_x = val_x
+        self.center_y = val_y
+        self.color = color
+    
+    def draw(self):
+        arcade.draw_triangle_filled(self.center_x + 40, self.center_y,
+                                    self.center_x, self.center_y - 100,
+                                    self.center_x + 80, self.center_y - 100,
+                                    arcade.color.WHITE)
+
+
+    def triangle_follow(self,val_x,val_y):
+
+        start_x = self.center_x
+        start_y = self.center_y
+
+        # Get the destination location for the bullet
+        try:
+            dest_x = val_x
+            dest_y = val_y
+
+            # Do math to calculate how to get the bullet to the destination.
+            # Calculation the angle in radians between the start points
+            # and end points. This is the angle the bullet will travel.
+            x_diff = dest_x - start_x
+            y_diff = dest_y - start_y
+            angle = math.atan2(y_diff, x_diff)
+
+            # Taking into account the angle, calculate our change_x
+            # and change_y. Velocity is how fast the bullet travels.
+            self.change_x = math.cos(angle) * SHIP_SPEED
+            self.change_y = math.sin(angle) * SHIP_SPEED
+
+            self.center_x += self.change_x
+            self.center_y += self.change_y
+
+            angle = 360-math.atan2(dest_x-300,dest_y-400)*180/math.pi
+
+            self.angle = angle
+        except Exception as e:
+            pass
+     
+
         
 class MyGame(arcade.Window):
     """
@@ -131,16 +216,23 @@ class MyGame(arcade.Window):
         # Variables that will hold sprite lists
         self.player_list = None
         self.coin_list = None
+        self.bullet_list = None
 
         # Set up the player info
         self.player_sprite = None
         self.score = 0
 
+        self.gun_sound = arcade.sound.load_sound(":resources:sounds/laser1.wav")
+        self.hit_sound = arcade.sound.load_sound(":resources:sounds/phaseJump1.wav")
+
+
         # Set the background color
         arcade.set_background_color(arcade.color.BLACK)
 
-        self.val_x = 400
-        self.val_y = 300
+        self.val_x = None
+        self.val_y = None
+
+        # self.triangle = Triangle(50,50,arcade.color.WHITE)
 
     def setup(self):
         """ Set up the game and initialize the variables. """
@@ -148,6 +240,7 @@ class MyGame(arcade.Window):
         # Sprite lists
         self.player_list = arcade.SpriteList()
         self.coin_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
 
         self.score = 0
 
@@ -161,8 +254,8 @@ class MyGame(arcade.Window):
         for i in range(COIN_COUNT):
             # Create the coin instance
             # Coin image from kenney.nl
-            coin = Coin(":resources:images/items/coinGold.png", SPRITE_SCALING_COIN)
-
+            # coin = Coin(":resources:images/items/coinGold.png", SPRITE_SCALING_COIN)
+            coin = Coin(':resources:images/space_shooter/meteorGrey_big3.png',SPRITE_SCALING_COIN)
             # Position the coin
             coin.center_x = random.randrange(SCREEN_WIDTH)
             coin.center_y = random.randrange(SCREEN_HEIGHT)
@@ -181,27 +274,53 @@ class MyGame(arcade.Window):
         # Draw all the sprites.
         self.player_list.draw()
         self.coin_list.draw()
-        self.draw_triangle_filled(50,50,50,arcade.color.AIR_SUPERIORITY_BLUE,2,20)
+        self.bullet_list.draw()
+        # self.triangle.draw()
 
         # Put the text on the screen.
-        output = f"Score: {self.score}"
+        output = f"Damage: {self.score}"
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+        if self.score == 5:
+            print(self.tracking_table)
+            time.sleep(1)
+            print('you fucked up')
+            time.sleep(1)
+            # return self.tracking_table
+            sys.exit()
 
+   
     def on_update(self, delta_time):
         """ Movement and game logic """
 
         # Call update on all sprites (The sprites don't do much in this
         # example though.)
+      
         self.player_list.update()
+        self.bullet_list.update()
+        # self.triangle.triangle_follow(self.val_x,self.val_y)
         # self.move_sprite()
-        self.follow_mouse()
+
+        for bullet in self.bullet_list:
+            hit_list = arcade.check_for_collision_with_list(bullet,self.coin_list)
+
+            if len(hit_list) > 0:
+                bullet.remove_from_sprite_lists()
+
+            for coin in hit_list:
+                coin.remove_from_sprite_lists()
+                
+            if bullet.bottom > self.width or bullet.top < 0 or bullet.right < 0 or bullet.left > self.width:
+                bullet.remove_from_sprite_lists()
+
+        for sprite in self.player_list:
+            sprite.follow_mouse(self.val_x,self.val_y)
 
 
         for i,coin in enumerate(self.coin_list):
             coin.follow_sprite(self.player_sprite)
             
             self.tracking_table[f'coin {i}'] = coin.get_coor()
-            print(f'{i} coin {self.coin_list._get_center()}')
+            # print(f'{i} coin {self.coin_list._get_center()}')
            
         # Generate a list of all sprites that collided with the player.
         hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
@@ -222,21 +341,68 @@ class MyGame(arcade.Window):
 
         # Rotate left/right
         elif key == arcade.key.LEFT:
+            self.val_x = None
             self.player_sprite.change_angle = ANGLE_SPEED
+           
         elif key == arcade.key.RIGHT:
+            self.val_y = None
             self.player_sprite.change_angle = -ANGLE_SPEED
+
+    def on_mouse_press(self,x,y,button,modifiers):
+
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            self.val_x = x
+            self.val_y = y
+
+        bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", SPRITE_SCALING_LASER)
+
+        start_x = self.player_sprite.center_x
+        start_y = self.player_sprite.center_y
+
+        bullet.center_x = start_x
+        bullet.center_y = start_y
+
+        dest_x = x
+        dest_y = y
+
+        x_diff = dest_x - start_x
+        y_diff = dest_y - start_y
+
+        angle = math.atan2(y_diff,x_diff)
+
+        bullet.angle = math.degrees(angle)
+
+        bullet.change_x = math.cos(angle) * BULLET_SPEED
+        bullet.change_y = math.sin(angle) * BULLET_SPEED
+
+        self.bullet_list.append(bullet)
+     
+   
+    def on_mouse_motion(self,x,y,dx,dy):
+        
+        print(x,y)
+        angle = 360-math.atan2(y-300,x-400)*480/math.pi
+
+        # dest_x = x
+        # dest_y = y
+
+        # start_x = self.player_sprite.center_x
+        # start_y = self.player_sprite.center_y
+
+        # # Do math to calculate how to get the bullet to the destination.
+        # # Calculation the angle in radians between the start points
+        # # and end points. This is the angle the bullet will travel.
+        # x_diff = dest_x - start_x
+        # y_diff = dest_y - start_y
+        # angle = math.atan2(y_diff, x_diff)
+
+        self.player_sprite.angle = angle
 
     # def on_mouse_motion(self,x,y,dx,dy):
 
     #     self.val_x = x
     #     self.val_y = y
 
-    def on_mouse_press(self,x,y,button,modifiers):
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            self.val_x = x
-            self.val_y = y
-    
-    
     # def move_sprite(self):
 
     #     x_dist = self.val_x - self.player_sprite._get_position()[0]
@@ -248,48 +414,11 @@ class MyGame(arcade.Window):
     #         self.player_sprite.center_x += x_dist * 0.1
     #         self.player_sprite.center_y += y_dist * 0.1
 
-    def follow_mouse(self):
-       
-
-        start_x = self.player_sprite.center_x
-        start_y = self.player_sprite.center_y
-
-        # Get the destination location for the bullet
-        
-        dest_x = self.val_x
-        dest_y = self.val_y
-
-        # Do math to calculate how to get the bullet to the destination.
-        # Calculation the angle in radians between the start points
-        # and end points. This is the angle the bullet will travel.
-        x_diff = dest_x - start_x
-        y_diff = dest_y - start_y
-        angle = math.atan2(y_diff, x_diff)
-
-        # Taking into account the angle, calculate our change_x
-        # and change_y. Velocity is how fast the bullet travels.
-        self.change_x = math.cos(angle) * SHIP_SPEED
-        self.change_y = math.sin(angle) * SHIP_SPEED
-
-        self.player_sprite.center_x += self.change_x
-        self.player_sprite.center_y += self.change_y
-
-        #360, 300,400, 180
-        angle = 360-math.atan2(self.val_x-300,self.val_y-400)*180/math.pi
-        
-        
-        
-        """
-        ENABLE THE NEXT LINE TO ROTATE BY MOUSE CLICK, THIS IS NOT FINISHED YET, I STILL GOTTA EXPLORE MORE!
-        """
-        self.player_sprite.angle = angle
+    ########################################
 
 
-        
-
-        
-
-        
+ 
+               
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key. """
 
@@ -300,9 +429,7 @@ class MyGame(arcade.Window):
     
     def get_table(self):
         print(self.tracking_table)
-        return self.tracking_table
-
-   
+        return self.tracking_table  
 
 
 def main():
